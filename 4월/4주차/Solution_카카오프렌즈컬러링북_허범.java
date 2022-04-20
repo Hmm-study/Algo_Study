696a2603471e9042546ba701bbbc3ccf68c06967
Elston <elston0425@naver.com>
2022-04-20 21:56:35 +0900

    0420 허범

--- 4월/4주차/Solution_카카오프렌즈컬러링북_허범.java
+++ 4월/4주차/Solution_카카오프렌즈컬러링북_허범.java
@@ -2,34 +2,34 @@ package 프로그래머스;
 
 public class 카카오프렌즈컬러링북 {
 
-    static int max;
-    static int maxCount;
-    static int count = 0;
+    static int numberOfArea;
+    static int maxSizeOfOneArea;
+    static int temp_cnt = 0;
     static int[] dx = {-1,1,0,0};
     static int[] dy = {0,0,-1,1};
 
     public int[] solution(int m, int n, int[][] picture) {
-        max =0;
-        maxCount =0;
+        numberOfArea =0;
+        maxSizeOfOneArea=0;
         int[] answer = new int[2];
-        answer[0] = max;
-        answer[1] = maxCount;
+        answer[0] = numberOfArea;
+        answer[1] = maxSizeOfOneArea;
 
         boolean[][] check = new boolean[m][n];
 
         for(int i =0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(picture[i][j] != 0 && !check[i][j]){
-                    max++;
+                    numberOfArea++;
                     dfs(i,j,picture,check);
                 }
-                if(count > maxCount) maxCount = count;
-                count = 0;
+                if(temp_cnt > maxSizeOfOneArea) maxSizeOfOneArea = temp_cnt;
+                temp_cnt = 0;
             }
         }
 
-        answer[0] = max;
-        answer[1] = maxCount;
+        answer[0] = numberOfArea;
+        answer[1] = maxSizeOfOneArea;
 
         return answer;
     }
@@ -38,7 +38,7 @@ public class 카카오프렌즈컬러링북 {
         if(check[x][y]) return;
 
         check[x][y] = true;
-        count++;
+        temp_cnt++;
 
         for(int i =0;i<4;i++){
             int nx = x + dx[i];
