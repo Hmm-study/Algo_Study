66819a22a9dac54ecd7b2588edd42f2901820870
unknown <ufoyws@naver.com>
2022-06-29 18:12:34 +0900

    0629 유우식

---
+++ 6월/5주차/Main_단어정렬_유우식.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class BOJ_1181 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        String[] word = new String[N];
+        for(int i=0; i<N; i++){
+            word[i] = br.readLine();
+        }
+
+        Arrays.sort(word, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if(o1.length() == o2.length()){
+                    return o1.compareTo(o2);
+                }
+                else{
+                    return o1.length() - o2.length();
+                }
+            }
+        });
+        System.out.println(word[0]);
+        for(int i=1; i<N; i++){
+            if(!word[i].equals(word[i-1])){
+                System.out.println(word[i]);
+            }
+        }
+    }
+}
