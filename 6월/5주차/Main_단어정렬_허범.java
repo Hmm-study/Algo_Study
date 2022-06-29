2f8ca82719f6f1e2c43149d9eed79fe1522b1359
Elston <elston0425@naver.com>
2022-06-29 16:43:52 +0900

    0629 허범

---
+++ 6월/5주차/Main_단어정렬_허범.java
@@ -0,0 +1,39 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class 단어정렬_1181 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        String[] words = new String[n];
+
+        for(int i = 0; i < n; i++){
+            words[i] = br.readLine();
+        }
+
+        Arrays.sort(words, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if(o1.length() == o2.length()){
+                    return o1.compareTo(o2);
+                }else{
+                    return o1.length() - o2.length();
+                }
+            }
+        });
+
+        System.out.println(words[0]);
+        for(int i = 1; i < n; i++){
+            if(!words[i].equals(words[i-1])){
+                System.out.println(words[i]);
+            }
+        }
+
+    }
+}
